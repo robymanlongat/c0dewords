a2f5c55d81001d5537aa873627693cf8ccf0d3a5
robymanlongat <68985185+robymanlongat@users.noreply.github.com>
2020-09-18 23:54:53 +1000

    Add files via upload

---
+++ week02/R/R.js
@@ -0,0 +1,22 @@
+function setup() {
+createCanvas (500,500);
+background (100,100,220);
+
+stroke (255);
+strokeWeight (5);
+}
+
+
+function draw() {
+  
+  stroke (200,200,150);
+  fill (250,100,0);
+  ellipse (200,150,250);
+
+  stroke (255);
+  fill (255);
+  rect (150,100,50,300); //x,y,width,height)
+  stroke (30,100,50);
+  line (0,150,500,450);
+
+}
