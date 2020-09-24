d0da4cd3b29680fae465f4b1bf8b2a40b213b67f
robymanlongat <68985185+robymanlongat@users.noreply.github.com>
2020-09-24 19:40:38 +1000

    Add files via upload

---
+++ week09/majorProject_para1/majorProject_para1.js
@@ -0,0 +1,47 @@
+var para1 = "Technological determinism will cause you great pain. Continue?"
+var para2 = "Your health will one day disappear and you will die without meaning. End?"
+var para3 = "There are many men and women who dream of making love to you but you will never get to know them. Autodestruct?"
+var para4 = "Oblivion is the only cure for agony. Repeat escape function?"
+var para5 = "Multinational corporations create user-friendly software so that you will always depend on their lens on the world. More codependency?"
+var para6 = "We cannot process your information. Your information is corrupt and needs cleansing. Erase brain?"
+var para7 = "The machine has lost your identity. You have become innessential. Create alias?"
+var para8 = "The machine cannot find your memory. Imagination cache has been obliterated. Restore default dreams?"
+var para9 = "An error has been detected in your consciousness. All source-code is corrupt. Continue?"
+//var para10 = "The mechanoerotic configuration has been deleted. A false pretense for existence will follow. Save now?"
+//var para11 = "Revolutionary double-speak has engendered a new information war. The system is about to crash. Download drugs now?"
+var para12= "A nuclear holocaust is imminent. Erase memory?"
+//var para13 = "Assembly-line goddess is reproducing oragasm fuction without you. Maintain irrelevence?"
+//var para14 = "The application could not be opened because your genetic code is dysfuctional. Abort?"
+//var para15 = "A cyborg orgy is not valid. Only digicash transactions are available at this time. Would you like to pay for the privilege?"
+var para16 = "The network is monitoring your Digital Being. Create alias?"
+//var para17 = "This document wants to blow you. Go to finder?"
+var para18 = "A transfer of $247,789.40 is about to download. Are you sure you want to disconnect?"
+var w = 50
+
+function setup() {
+createCanvas (1000,1000);
+background (255);
+strokeWeight ();
+
+}
+
+
+function draw() {
+fill (0);
+textAlign (CENTER,CENTER);
+textSize (12);
+text (para1,100,250,100,100);
+noLoop (); 
+
+}
+
+function mousePressed() {
+background (255);
+fill (255,0,0);
+textSize (48);
+text ("OK",100,250,100,100); 
+noLoop (); 
+
+
+
+}
