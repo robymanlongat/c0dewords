d6c37816c4e18be0bd967b4ce0a105ef6ae04079
robymanlongat <68985185+robymanlongat@users.noreply.github.com>
2020-09-28 00:05:57 +1000

    Add files via upload

---
+++ week09.5/majorProject_para3_updated/majorProject_para3_updated.js
@@ -0,0 +1,484 @@
+let mode = 0;
+
+function setup() {
+  createCanvas(1000,700);
+  background (255);
+  strokeWeight ();
+  
+}
+
+
+function draw() {
+
+  switch (mode) {
+    case 0:
+      background(255);
+      break;
+    case 1:
+      scene1();
+      break;
+    case 2:
+      scene2();
+      break;
+    case 3:
+      scene3();
+      break;
+    case 4:
+      scene4();
+      break;
+    case 5:
+      scene5();
+      break;
+    case 6:
+      scene6();
+      break;
+    case 7:
+      scene7();
+      break;
+    case 8:
+      scene8();
+      break;
+    case 9:
+      scene9();
+      break;
+    case 10:
+      scene10();
+      break;
+     case 11:
+      scene11();
+      break;
+     case 12:
+      scene12();
+      break;
+     case 13:
+      scene13();
+      break;
+     case 14:
+      scene14();
+      break;
+     case 15:
+      scene15();
+      break;
+     case 16:
+      scene16();
+      break;
+     case 17:
+      scene17();
+      break;
+      case 18:
+      scene18();
+      break;
+     case 19:
+      scene19();
+      break;
+     case 20:
+      scene20();
+      break;
+     case 21:
+      scene21();
+      break;
+      case 22:
+      scene22();
+      break;
+     case 23:
+      scene23();
+      break;
+    default:   
+    }
+}
+
+function mousePressed() {
+  mode++;
+  }
+
+function scene1() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+}
+
+function scene2() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+}
+
+function scene3() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);  
+}
+
+function scene4() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+}
+
+function scene5() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);  
+}
+
+function scene6() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+}
+
+function scene7() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+}
+
+function scene8() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+}
+
+function scene9() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+}
+
+function scene10() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+}
+
+function scene11() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+}
+
+function scene12() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+}
+
+function scene13() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+}
+
+function scene14() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+  text('but', 868, 415);
+}
+
+function scene15() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+  text('but', 868, 415);
+  text('you', 703, 430);
+}
+
+function scene16() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+  text('but', 868, 415);
+  text('you', 703, 430);
+  text('will', 725, 430);
+}
+
+function scene17() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+  text('but', 868, 415);
+  text('you', 703, 430);
+  text('will', 725, 430);
+  text('never', 745, 430);
+}
+
+function scene18() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+  text('but', 868, 415);
+  text('you', 703, 430);
+  text('will', 725, 430);
+  text('never', 745, 430);
+  text('get', 778, 430);
+}
+
+function scene19() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+  text('but', 868, 415);
+  text('you', 703, 430);
+  text('will', 725, 430);
+  text('never', 745, 430);
+  text('get', 778, 430);
+  text('to', 798, 430);
+}
+
+function scene20() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+  text('but', 868, 415);
+  text('you', 703, 430);
+  text('will', 725, 430);
+  text('never', 745, 430);
+  text('get', 778, 430);
+  text('to', 798, 430);
+  text('know', 811, 430);
+}
+
+function scene21() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+  text('but', 868, 415);
+  text('you', 703, 430);
+  text('will', 725, 430);
+  text('never', 745, 430);
+  text('get', 778, 430);
+  text('to', 798, 430);
+  text('know', 811, 430);
+  text('them.', 842, 430);
+}
+
+function scene22() {
+  background (255);
+  fill (0);
+  textSize (12);
+  text('There', 700, 400);
+  text('are', 735, 400);
+  text('many', 755, 400);
+  text('men', 787, 400);
+  text('and', 814, 400);
+  text('women', 838, 400);
+  text('who', 690, 415);
+  text('dream', 715, 415);
+  text('of', 752, 415);
+  text('making', 765, 415);
+  text('love', 807, 415);
+  text('to', 832, 415);
+  text('you', 845, 415);
+  text('but', 868, 415);
+  text('you', 703, 430);
+  text('will', 725, 430);
+  text('never', 745, 430);
+  text('get', 778, 430);
+  text('to', 798, 430);
+  text('know', 811, 430);
+  text('them.', 842, 430);
+  text('Autodestruct?', 750, 445);
+}
+
+function scene23() {
+  background (255);
+  fill (255,0,0);
+  textSize (48);
+  text('OK',750,435);
+}
